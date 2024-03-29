b5cd514eac647cdf3d29d67cd487901f7479c81f
larybino <binolaryssa@gmail.com>
2024-03-29 11:30:08 -0300

    Adicionando o frontend

--- src/main/java/me/dio/satanderdevweek2024/domain/exception/ChampionNotFoundException.java
+++ src/main/java/me/dio/satanderdevweek2024/domain/exception/ChampionNotFoundException.java
@@ -2,7 +2,7 @@ package me.dio.satanderdevweek2024.domain.exception;
 
 public class ChampionNotFoundException extends RuntimeException{
     public ChampionNotFoundException(long championId) {
-        //TODO Auto-generated constructor stub
+
         super("Champion %d not found.".formatted(championId));
 
     }
